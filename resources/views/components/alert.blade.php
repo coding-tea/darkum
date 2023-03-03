e307a8c65c039f0e0b7f0479aa5545b4967f9739
smarsi <soufianemarsi1999@gmail.com>
2023-03-03 20:22:20 +0100

    install laravel

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,5 @@
+<div>
+  @isset($title)
+      <h1>{{$title}}</h1>
+  @endisset
+</div>
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\Afficherformulaire;
+use App\Http\Controllers\CalculatriceController;
+use App\Http\Controllers\Inscription;
+use App\Http\Controllers\TretmentControllers;
+use App\Http\Controllers\SingleActionController;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*==================TP3========================= */
+//Exercice 1 :
+//-------Affichage des class controllers:
+
+  Route::view("/", "welcome");
+
+?>
