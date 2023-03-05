a252a9a5cdc287e70a3ad155dc2ff4dd10f85e6e
smarsi <soufianemarsi1999@gmail.com>
2023-03-05 19:20:35 +0100

    create database tables

---
+++ database/migrations/2023_03_05_172704_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+          $table->id();
+          $table->string("nomComplet", 100);
+          $table->string("email", 100)->unique();
+          $table->text("password");
+          $table->string("tel", 20);
+          $table->enum("role", ["admin", "user"])->default("user");
+          $table->text('adresse');
+          $table->string("retrievePassword");
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
