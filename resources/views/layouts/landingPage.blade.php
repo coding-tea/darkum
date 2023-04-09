6796520c9db827884b2cd75e8ca2bf24e141c1cb
smarsi <soufianemarsi1999@gmail.com>
2023-04-09 18:15:06 +0000

    working in landing page

--- resources/views/layouts/landingPage.blade.php
+++ resources/views/layouts/landingPage.blade.php
@@ -5,83 +5,95 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="{{asset('css/landing_page.css')}}">
+  <link rel="stylesheet" href="{{asset("css/landing_page.css")}}">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <title>@yield("title")</title>
 </head>
 <body>
+  {{----------------------- HEADER ---------------------------}}
+  
+  <nav class="navbar">
+    <div class="brand-title">
+      <img src="{{asset("img/logo.png")}}" alt="Logo" class="logoImg">
+      <span class="p-2 fw-semibold logoName"> Darkum</span>
+    </div>
 
-  {{----------------------- HEADER -------------------------------}}
+    <!-- This is for responsive menu -->
+    <a href="#" class="toggle-button">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+    </a>
 
-  <nav class="navbar navbar-expand-lg" style="background-color: #EEF1F6" data-bs-theme="dark">
-    <div class="container-fluid">
-      <a class="navbar-brand me-5" href="#">
-        <img src="{{asset("img/logo.png")}}" alt="Logo" width="40" height="40" class="d-inline-block align-text-top">
-        <span class="p-2 fw-semibold" style="color:#4A647F"> Darkum</span>
-      </a>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav mx-auto">
+    <!-- This is the link for the different pages -->
+    <div class="navbar-links">
+        <ul>
           <li class="nav-item">
             <a class="nav-link" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Vente</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
+            <a class="nav-link" href="#">Location</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" style="color:#4A647F" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-            </svg></a>
+            <a class="nav-link login" href="#"><i class="fa fa-user-circle-o"></i> se connecter</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Something</a>
+            <a class="nav-link signUp" href="#">s'inscrire</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">sign in</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link signUp" href="#">sign up</a>
-          </li>
-        
         </ul>
-
-
-        {{-- <form class="d-flex " role="search">
-          <input class="form-control me-2 bg-light" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-danger" type="submit">Search</button>
-        </form> --}}
       </div>
-    </div>
-  </nav>
+      <a class="btn nvAnnonce" href="#">PUBLIER UNE ANNONCE</a>
+</nav>
   
-  {{----------------------- BODY ---------------------------------}}
+  @section("content")
+    <section class="header-section">
+      <div class="publiciter">
+        
+          <p class="pubTitre h1">Trouvez Votre logement de rêve.</p>
+          <p class="pubParagraphe">
+            Vous cherchez un endroit à louer ? <br> Ne cherchez pas plus loin ! Notre site web est là pour vous aider.
+          </p>
+      </div>    
+        <div class="filterRecherche">
+          <div class="zone1">
+            <label>
+              <input type="radio" class="btnInput" name="filter" value="option1" checked>
+              <span class="btn-label" style="border-radius: 15px 1px 1px 15px">Vente</span>
+            </label>
+          
+            <label>
+                <input type="radio" class="btnInput" name="filter" value="option2">
+                <span class="btn-label" style="border-radius: 1px">Location</span>
+            </label>
+          
+            <label style="margin-right: 10px">
+                <input type="radio" class="btnInput" name="filter" value="option3">
+                <span class="btn-label" style="border-radius: 1px 15px 15px 1px">Immo neuf</span>
+            </label>
+          </div>
+          <div class="zone2">
+            <select name="select">
+              <option value="*">Tous</option>
+              <option value="maison">Maisons</option>
+              <option value="appartement">Appartements</option>
+              <option value="villa">Villas</option>
+              <option value="chambre">Chambres</option>
+            </select>
+            <span style="margin-right: 10px"></span>
+          </div>
+          <div class="zone3 d-flex">
+            <input type="text" name="searchVille" placeholder="Où cherchez-vous">
+            <button type="submit" class="btnRech">Rechercher</button>
+          </div>
+        </div>
+      
+    </section>
+  @show
 
-
-@section("landingPage")
-  <section class="interface mt-5 d-flex">
-    <div>
-      <h1 class="title">Darkum</h1>
-      <h2 class="pubTitre">Trouvez Votre logement de rêve.</h2>
-      <p class="pubParagraphe">
-        Vous cherchez un endroit à louer ? Ne cherchez pas plus loin ! Notre site web est là pour vous aider.
-      </p>
-      <button class="btn">Show More</button>
-    </div>
-    <div>
-      <img src="img/home.jpg" alt="homeImage" class="w-100 ms-5 mb-5">
-    </div>
-  </section>
-@show
-
-
-
-
-
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-  
+  <script src="{{asset("js/landing_page.js")}}"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
