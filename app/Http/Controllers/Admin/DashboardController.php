b476d6861c90ae1261b1d44dbcad4eaff79842fb
smarsi <soufianemarsi1999@gmail.com>
2023-05-26 20:01:25 +0100

    save my work in dashbord

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use SebastianBergmann\CodeCoverage\Report\Xml\Report;
 
 class DashboardController extends Controller
 {
@@ -15,7 +17,8 @@ class DashboardController extends Controller
       $user = Auth::user();
 
       if ($user->role === 'admin') {
-        return view('pages.admin.dashboard');
+        $nbUsers = User::count();
+        return view('pages.admin.dashboard', compact("nbUsers"));
       } else {
         return view('pages.user.index');
 
--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -18,8 +18,9 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                               User</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
+                               User
+                            </div>
+                            <div class="h4 mb-0 font-weight-bold text-gray-800">@isset($nbUsers){{$nbUsers}}@endisset</div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-hotel fa-2x text-gray-300"></i>
@@ -36,7 +37,8 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Balance</div>
+                                Repport
+                            </div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
                         </div>
                         <div class="col-auto">
